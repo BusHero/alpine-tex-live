ee8b9c2f676970745be4acd9718bb2ffa59d8f09
bus1hero <petru.cervac@gmail.com>
2022-12-18 13:40:57 +0200

    feat: create the first image

---
+++ smoke_tests/tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+file_exist=1
+
+cd tmp/
+pdflatex small2e
+if [ ! -f /tmp/small2e.pdf ]; then
+	echo "'pdflatex small2e' failed to create a pdf file"
+	file_exist=0
+fi
+rm -rf tmp/*
+
+if [ $file_exist -eq 0 ]; then
+	exit 1
+fi
